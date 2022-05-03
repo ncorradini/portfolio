eaed638ca4d5ee3a71e64a5c4602bee7c4548050
Nicolás Corradini <nicocorradini471@gmail.com>
2022-05-03 15:38:20 -0300

    Created ScrollTop component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import ToggleColorMode from './Layout/Layout';
 import Presentation from './components/Presentation';
+import AboutMe from './components/AboutMe';
+import Proyects from './components/Proyects';
+import Contact from './components/Contact';
+import ScrollTop from './components/ScrollTop';
+import { Fab, Toolbar } from '@mui/material';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const App = () => {
   return (
     <ToggleColorMode>
+      <Toolbar id="back-to-top-anchor" />
       <Presentation />
+      <AboutMe />
+      <Proyects />
+      <Contact />
+      <ScrollTop>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
     </ToggleColorMode>
   );
 };
---
+++ src/components/ScrollTop/index.js
@@ -0,0 +1,38 @@
+import { useScrollTrigger, Zoom, Box } from '@mui/material';
+
+const ScrollTop = props => {
+  const { children, window } = props;
+
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 20, right: 50 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+};
+
+export default ScrollTop;
