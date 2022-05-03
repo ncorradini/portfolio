6e970bc4a67a791687fd6ad4044cd4dd9a5126e5
Nicolás Corradini <nicocorradini471@gmail.com>
2022-05-03 11:31:47 -0300

    Fixed header and presentation component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,51 +1,51 @@
 import ToggleMode from './ToggleMode';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
 
 const Header = ({ colorMode, theme }) => {
-  const sections = ['aboutme', 'skills', 'proyects', 'contact'];
+  const sections = ['aboutme', 'proyects', 'contact'];
 
   const tradSection = text => {
     switch (text) {
       case 'aboutme': return 'Sobre mí';
-      case 'skills': return 'Skills';
       case 'proyects': return 'Proyectos';
       case 'contact': return 'Contacto';
     }
   };
 
   return (
-    <Box component="header" sx={{ display: 'flex', marginTop: '20px', px: '10px' }}>
-      <ToggleMode colorMode={colorMode} theme={theme} />
+    <Container maxWidth="md">
+      <Box component="header" sx={{ display: 'flex', marginTop: '20px', px: '10px' }}>
+        <ToggleMode colorMode={colorMode} theme={theme} />
 
-      {/*  Menu List */}
-      <Box sx={{
-        display: { xs: 'none', md: 'flex', sm: 'flex' },
-        width: '100%',
-        justifyContent: 'flex-end',
-      }}>
-        {sections.map((section) => (
-          <Button
-            size="small"
-            key={section}
-            color="secondary"
-            disableRipple="true"
-            href={`#${section}`}
-            sx={{
-              my: 2,
-              px: 2,
-              display: 'block',
-              transition: '0.5s',
-              '&.MuiButtonBase-root:hover': {
-                bgcolor: 'transparent',
-                opacity: '0.5',
-              },
-            }}>
-            {tradSection(section)}
-          </Button>
-        ))}
+        {/*  Menu List */}
+        <Box sx={{
+          display: { md: 'flex', sm: 'flex', xs: 'flex' },
+          width: '100%',
+          justifyContent: 'flex-end',
+        }}>
+          {sections.map((section) => (
+            <Button
+              size="small"
+              key={section}
+              color="secondary"
+              disableRipple="true"
+              href={`#${section}`}
+              sx={{
+                my: 2,
+                px: 2,
+                display: 'block',
+                transition: '0.5s',
+                '&.MuiButtonBase-root:hover': {
+                  bgcolor: 'transparent',
+                  opacity: '0.5',
+                },
+              }}>
+              {tradSection(section)}
+            </Button>
+          ))}
+        </Box>
       </Box>
-
-    </Box>
+    </Container>
   );
 };
 
--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -1,26 +1,26 @@
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
 import Personal from './Personal';
 import notebookSVG from '../../assets/notebook.svg';
 
 const Presentation = () => {
   return (
-    <Box component="section" sx={{
-      display: 'flex',
-      alignItems: 'center',
-      height: '500px',
-      p: '20px',
-    }}>
-      <Personal />
-      <Box sx={{
-        width: '350px',
-        mt: '40px',
+    <Container maxWidth="md">
+      <Box component="section" sx={{
+        display: 'flex',
+        alignItems: 'center',
+        height: '500px',
+        p: '20px',
       }}>
-
-        <img
-          src={notebookSVG}
-        />
+        <Personal />
+        <Box sx={{
+          display: { lg: 'block', md: 'block', sm: 'block', xs: 'none' },
+          width: '350px',
+          mt: '40px',
+        }}>
+          <img src={notebookSVG}/>
+        </Box>
       </Box>
-    </Box>
+    </Container>
   );
 };
 
